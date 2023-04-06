d5e8da98931178b354e1114ec8b9231ba8480711
Ze Mario Guerra <59294353+jmarioguerraf@users.noreply.github.com>
2023-04-06 17:28:00 -0300

    Add files via upload
    That's is the archive.

---
+++ BOTZEH 2.py
@@ -0,0 +1,30 @@
+import time
+
+start():
+    
+print ("Digite sua idade:")
+idade = int(input ( ))
+time.sleep (1)
+print (idade,("?"))
+time.sleep (0.5)
+if idade < 18 :
+    print ("Você é uma criança. Solte o celular e vá brincar!")
+elif idade in range(18, 59) :
+    print ("Tudo bem. Seja bem-vindo!")
+elif idade in range(60, 100000000000) :
+    print ("Tudo bem. Você é adulto, mas não demore muito para dormir.")
+else :
+    print ("Informação Inválida")
+time.sleep (0.5)
+
+print ("Avaliação Concluída")
+
+While:
+    start()
+
+# while :
+#     print ("Avaliação Concuída.")
+
+# time.sleep (1)
+
+# start( )
